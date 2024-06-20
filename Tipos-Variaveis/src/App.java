a8d45970c81c51db5966474c48926d8ef25e8d71
maycondev11 <maycondev11@hotmail.com>
2024-06-19 21:59:42 -0300

    atualizado

--- Tipos-Variaveis/src/App.java
+++ Tipos-Variaveis/src/App.java
@@ -1,9 +1,9 @@
-public class App {
+public class App   {
     public static void main(String[] args) throws Exception {
 
         int valor= 10 + 20;
         double carro= 30.6 * 30.2;
-        String nome= "maycon" + "leite"; 
+        String nome= "Linguagem"  + " JAVA"; 
         System.out.println(nome);
         System.out.println(carro);
         System.out.println(valor);
