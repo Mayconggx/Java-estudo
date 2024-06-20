b3aeadf878ca26832eac01fb6b6458ae1ac04b0b
maycondev11 <maycondev11@hotmail.com>
2024-06-19 21:43:57 -0300

    RENOME

---
+++ Tipos-Variaveis/src/tipos variaveis.java
@@ -0,0 +1,12 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+
+        int valor= 10 + 20;
+        double carro= 30.6 * 30.2;
+        String nome= "maycon" + "leite"; 
+        System.out.println(nome);
+        System.out.println(carro);
+        System.out.println(valor);
+        
+    }
+}
